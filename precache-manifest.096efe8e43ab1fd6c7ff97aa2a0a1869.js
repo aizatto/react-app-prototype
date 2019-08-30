538cc244ffd2f985fddcea4ace16b5a9f3e7ba66
Aizat Faiz <aizat.faiz@gmail.com>
2019-08-30 22:28:08 +0800

    Updates

---
+++ precache-manifest.096efe8e43ab1fd6c7ff97aa2a0a1869.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b54c874b74a28d3aafb6e4168f668b7",
+    "url": "/react-app-prototype/index.html"
+  },
+  {
+    "revision": "552398bc7511ee2d7d39",
+    "url": "/react-app-prototype/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "e0956e198383f139fd71",
+    "url": "/react-app-prototype/static/js/2.d8d9c7d2.chunk.js"
+  },
+  {
+    "revision": "552398bc7511ee2d7d39",
+    "url": "/react-app-prototype/static/js/main.5f5dbf3d.chunk.js"
+  },
+  {
+    "revision": "bd9699862e788739e5e1",
+    "url": "/react-app-prototype/static/js/runtime~main.a581c05d.js"
+  }
+]);
